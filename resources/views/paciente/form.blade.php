07ef75c6d6a41c5c4ee2bb7a87018eb88d988af7
elgimass <lmassellag@miumg.edu.gt>
2021-08-28 11:38:51 -0600

    Profcile

--- resources/views/paciente/form.blade.php
+++ resources/views/paciente/form.blade.php
@@ -3,33 +3,33 @@
 <div class="container">
 <div class="card">
   <div class="card-body">
-      
+
         <div class ="form-group">
-            <div class="input-group">                
-                <input class="input-group" type="text" name="Nombre" value="{{ isset ($paciente->nombre)?$paciente->nombre:''}}" id="nombre" placeholder="Ingresar Nombre">
-                
-            </div> 
+            <div class="input-group">
+                <input class="input-group" type="text" name="nombre" value="{{ isset ($paciente->nombre)?$paciente->nombre:''}}" id="nombre" placeholder="Ingresar Nombre">
+
+            </div>
             <br>
             <div class="input-group">
-                 <input class="input-group" type="number" name="Edad" value="{{isset ($paciente->edad)?$paciente->edad:''}}" id="edad" placeholder="Ingresar Edad">
-            </div> 
-            <br>  
+                 <input class="input-group" type="number" name="edad" value="{{isset ($paciente->edad)?$paciente->edad:''}}" id="edad" placeholder="Ingresar Edad">
+            </div>
+            <br>
             <div class="input-group">
             <input class="input-group" type="text" name="direccion" value="{{isset ($paciente->direccion)?$paciente->direccion:''}}" id="direccion"  placeholder="Ingresar Direccion">
-            </div>     
-            <br>  
+            </div>
+            <br>
             <div class="input-group">
             <input class="input-group" type="text" name="telefono" value="{{isset ($paciente->telefono)?$paciente->telefono:''}}" id="telefono" placeholder="Ingresar Telefono">
-            </div>     
-            <br>  
+            </div>
+            <br>
             <div class="input-group">
             <input class="input-group" type="text" name="ocupacion" value="{{isset ($paciente->ocupacion)?$paciente->ocupacion:''}}" id="ocupacion" placeholder="Ingresar Ocupacion">
-            </div> 
-            <br>  
+            </div>
+            <br>
             <div class="input-group">
             <input class="input-group" type="text" name="referido" value="{{isset ($paciente->referido)?$paciente->referido:''}}" id="referido" placeholder="Ingresar Referido">
-            </div> 
-        </div>  
+            </div>
+        </div>
   </div>
 </div>
 
