f2b353d1f8b000e0fed8ea9e76abe83fbe7fba3d
elgimass <lmassellag@miumg.edu.gt>
2021-08-29 21:42:26 -0600

    prueba tratamientos

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -50,10 +50,10 @@ class CitaController extends Controller
     {
         $datos_paciente = Paciente::all();
         $datos_tratamiento = Tratamiento::all();
-        return view('Cita.create',[
+        return view ('Cita.create',compact([
             'pacientes'  => $datos_paciente,
-            //'tratamientos' => $datos_tratamiento
-        ]);
+            'tratamientos' => $datos_tratamiento
+        ]));
     }
 
     /**
