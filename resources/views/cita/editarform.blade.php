8319c3f501ed77abb9a50f4dfb12457528415656
elgimass <lmassellag@miumg.edu.gt>
2021-09-08 23:53:04 -0600

    cuenta y pieza citas

--- resources/views/cita/editarform.blade.php
+++ resources/views/cita/editarform.blade.php
@@ -34,6 +34,43 @@
             </div>
             <br>
             <div class="input-group">
+                <label for="pieza"> Seleccionar el número de pieza a tratar </label>
+            <select name="pieza" class="input-group" value="{{isset ($citas[0]->pieza)?$citas[0]->pieza:''}}" id="pieza">
+                select name="" id="" >
+                <option value="{{$citas[0]->estado}}">{{ $citas[0]->estado}}</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+    <option value="13">13</option>
+    <option value="14">14</option>
+    <option value="15">15</option>
+    <option value="16">16</option>
+    <option value="17">17</option>
+    <option value="18">18</option>
+    <option value="19">19</option>
+    <option value="20">20</option>
+    <option value="21">21</option>
+    <option value="22">22</option>
+    <option value="23">23</option>
+    <option value="24">24</option>
+    <option value="25">25</option>
+    <option value="26">26</option>
+    <option value="27">27</option>
+    <option value="28">28</option>
+    <option value="29">29</option>
+    <option value="30">30</option>
+    <option value="31">31</option>
+    <option value="32">32</option>
+              </select>
             <input class="input-group" type="text" name="pieza" value="{{isset ($citas[0]->pieza)?$citas[0]->pieza:''}}" id="pieza" placeholder="Ingresar Pieza">
             </div>
             <br>
